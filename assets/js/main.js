ce2409903681330645ee8277c8d0a25afce50048
Florian Köhler <git@flokoe.de>
2025-05-08 23:38:07 +0200

    Add simple toc

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,14 @@ import Alpine from 'alpinejs'
 
 window.Alpine = Alpine
 
+Alpine.data('closeToc', () => ({
+  closeToc(event) {
+    // Only act if an <a> tag was clicked
+    const target = event.target.closest('a');
+    if (target) {
+      this.showToc = !this.showToc
+    }
+  }
+}))
+
 Alpine.start()
